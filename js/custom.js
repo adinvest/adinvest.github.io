90e31da367eadc2f13a325b891781855ed659061
adinvest <88491600+adinvest@users.noreply.github.com>
2021-12-07 03:30:48 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,97 @@
-var topbutton = document.getElementById("topBtn");
+(function($) {
 
-      window.onscroll = function () {
-        scrollFunction();
-      };
+    "use strict";
+	
+    $(document).ready(function() {
+		
+		// PRELOADER
+        $("body").toggleClass("loaded");
+        setTimeout(function() {
+            $("body").addClass("loaded");
+        }, 3000);
+		
+		// PORTFOLIO DIRECTION AWARE HOVER EFFECT
+		var item = $("#bl-work-items>div");
+		var elementsLength = item.length;
+		for (var i = 0; i < elementsLength; i++) {
+			$(item[i]).hoverdir();
+		}
+		
+		// TEXT ROTATOR
+		$("#selector").animatedHeadline({
+             animationType: "clip"
+        });
+		
+		// BOX LAYOUT
+        Boxlayout.init();
+		
+		// REMOVE # FROM URL
+		$("a[href='#']").on("click", (function(e) {
+			e.preventDefault();
+		}));
 
-      function scrollFunction() {
-        if (
-          document.body.scrollTop > 150 ||
-          document.documentElement.scrollTop > 150
-        ) {
-          topbutton.style.display = "block";
-        } else {
-          topbutton.style.display = "none";
+        // AJAX CONTACT FORM
+        $(".contactform").on("submit", function() {
+            $(".output_message").text("Загрузка...");
+
+            var form = $(this);
+            $.ajax({
+                url: form.attr("action"),
+                method: form.attr("method"),
+                data: form.serialize(),
+                success: function(result) {
+                    if (result == "success") {
+                        $(".contactform").find(".output_message").addClass("success");
+                        $(".output_message").text("Отправлено!");
+                    } else {
+                        $(".contactform").find(".output_message").addClass("error");
+                        $(".output_message").text("Ошибка!");
+                    }
+                }
+            });
+
+            return false;
+        });
+
+        // MAX-LENGTH
+        function isNotMax(e){
+            e = e || window.event;
+            var target = e.target || e.srcElement;
+            var code=e.keyCode?e.keyCode:(e.which?e.which:e.charCode)
+        
+            switch (code){
+                case 13:
+                case 8:
+                case 9:
+                case 46:
+                case 37:
+                case 38:
+                case 39:
+                case 40:
+                return true;
+            }
+            return target.value.length <= target.getAttribute('maxlength');
         }
-      }
 
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
+		// MATERIAL CAROUSEL
+        $(".carousel.carousel-slider").carousel({
+            fullWidth: true,
+            indicators: true,
+        });
+		
+		// RESUME CARDS ANIMATION
+		if ($(window).width() > 800) {
+			$(".resume-list-item, .resume-card").on("click", function() {
+				$(".resume-list-item").removeClass("is-active");
+				var e = parseInt($(this).data("index"),10);
+				$("#resume-list-item-" + e).addClass("is-active");
+				var t = e + 1,
+					n = e - 1,
+					i = e - 2;
+				$(".resume-card").removeClass("front back up-front up-up-front back-back"), $(".resume-card-" + e).addClass("front"), $(".resume-card-" + t).addClass("back"), $(".resume-card-" + n).addClass("back-back"), $(".resume-card-" + i).addClass("back")
+			});
+		}
+		
+    });
 
-
-
+})(jQuery);
