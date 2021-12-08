63a3f394dbc919c4d1619e0d6659685912880137
adinvest <88491600+adinvest@users.noreply.github.com>
2021-12-08 11:19:11 +0300

    Update process-form.php

--- php/process-form.php
+++ php/process-form.php
@@ -14,7 +14,7 @@ if (isset($_REQUEST['name'],$_REQUEST['email'])) {
     $email = trim($email);
     $message  = trim($message);
      
-    $to = 'adinvest@list.com';
+    $to = 'adinvest@list.ru';
       
     $subject = 'Contact Request From Website';
     $headers = "From: ".$name." <".$email."> \r\n";
@@ -23,5 +23,29 @@ if (isset($_REQUEST['name'],$_REQUEST['email'])) {
       
     echo ($send_email) ? 'success' : 'error';
       
+}
+
+
+$recaptcha = $_POST['g-recaptcha-response'];
+ 
+if(!empty($recaptcha)) {
+    $recaptcha = $_REQUEST['g-recaptcha-response'];
+    $secret = '6LdKkIIdAAAAAFKX8l3wyYiViQWWXtvaFlDFYhA-';
+    $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secret ."&response=".$recaptcha."&remoteip=".$_SERVER['REMOTE_ADDR'];
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:54.0) Gecko/20100101 Firefox/54.0");
+    $curlData = curl_exec($curl);
+    curl_close($curl); 
+    $curlData = json_decode($curlData, true);
+    if($curlData['success']) {
+    } else {
+        echo "Подтвердите, что вы не робот и попробуйте еще раз";
+    }
+}
+else {
+    echo "поставьте галочку в поле 'Я не робот' для отправки сообщения";
 }
 ?>
